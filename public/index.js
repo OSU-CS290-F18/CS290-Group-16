868dcaf47167656293c67291b558b175c0476670
elebid <41494508+elebid@users.noreply.github.com>
2018-11-28 22:43:45 -0800

    Added add task button

--- public/index.js
+++ public/index.js
@@ -1,3 +1,15 @@
+$(document).ready(function() {
+    $(":text").keyup(function(e) {
+        if($(this).val() != '') {
+            $(":text").not(this).attr('disabled','disabled');
+            $(this).next("input").show();
+        } else {
+            $(":text").removeAttr('disabled');
+            $(this).next("input").hide();
+        }
+    });
+});
+
 var acc = document.getElementsByClassName("accordion");
 var panel = document.getElementsByClassName('panel');
 var lines = document.getElementById("lines")
@@ -22,4 +34,59 @@ function setClass(els, className, fnName) {
 }
 
 //panel.style.maxHeight = panel.scrollHeight + "px";
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+var textBox = document.getElementById("task-input");
+textBox.addEventListener("click", displayAdd);
+
+function displayAdd() {
+
+    var addTButton = document.getElementById("addTask");
+    addTButton.style.display === "block";
+};
+
+function myFunction() {
+    alert ("Hello World!");
+}
+
+var allTasks = [];
+
+function insertNewTask(task) {
+
+    var insertTaskContext = {
+      
+      task: task
+      };
+      
+      var taskHTML = Handlebars.templates.insertTask(insertTaskContext); 
+      
+    var insertTaskContainer = document.getElementById("list");
+    insertTaskContainer.insertAdjacentHTML("beforeend", taskHTML);
+
+};
+
+function HandleAddTask() {
+
+    var task = document.getElementById('task-input').value.trim();
+  
+    if (!task) {
+      alert("You must enter a task");
+    } else {
+  
+      allTasks.push({
+        task: task
+      });
+  
+      clearTaskField();
+    }
+  }
+
+  function clearTaskField() {
+
+    document.getElementById('task-input').value = "";
+  }
+
+  var taskEnter = document.getElementById('addTask');
+  if (taskEnter) {
+    taskEnter.addEventListener('click', taskEnter);
+  };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 
-//var data = require('./tasksData');
+//var data = require('./taskData');
 
 var app = express();
 var port = process.env.PORT || 3000;
