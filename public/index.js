b26d7b3b5d8f1600e8684e5e9969776a48ecca34
Matthew Laidlaw <matt@10-248-67-90.wireless.oregonstate.edu>
2018-11-28 20:07:03 -0800

    Templatized html, added package.json, updated index.js and created server.js

--- public/index.js
+++ public/index.js
@@ -9,6 +9,18 @@ for (i = 0; i < acc.length; i++) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+    }
   });
 }
+
+var inputTaskLine = document.querySelector("#task-input");
+inputTaskLine.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    var newTaskContext = {
+      description: inputTaskLine.value
+    }
+    var newTaskHTML = Handlebars.templates.task(newTaskContext);
+    inputTaskLine.insertAdjacentHTML("beforebegin", newTaskHTML);
+  }
+});
