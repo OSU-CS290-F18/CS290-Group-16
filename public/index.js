f81a2c8b01947dcd849ed065c3cacb01dc8f5ce4
Matthew Laidlaw <matt@10-248-67-90.wireless.oregonstate.edu>
2018-12-02 18:09:15 -0800

    Updated handlebars partials, updated server.js to connect to mongodb database, client side js now creates post requests on button clicks so that not only will the page update, the database will update too, installed body-parser dependency, made slight changes to styling of page

--- public/index.js
+++ public/index.js
@@ -17,10 +17,106 @@ var inputTaskLine = document.querySelector("#task-input");
 inputTaskLine.addEventListener("keyup", function(event) {
   event.preventDefault();
   if (event.keyCode === 13) {
-    var newTaskContext = {
+    var postRequest = new XMLHttpRequest();
+    var requestURL = '/insertTask';
+    postRequest.open('POST', requestURL);
+
+    var requestBody = JSON.stringify({
       description: inputTaskLine.value
-    }
-    var newTaskHTML = Handlebars.templates.task(newTaskContext);
-    inputTaskLine.insertAdjacentHTML("beforebegin", newTaskHTML);
+    });
+
+    postRequest.addEventListener('load', function (event) {
+      if (event.target.status === 200) {
+        var newTaskContext = {
+          description: inputTaskLine.value
+        }
+        var newTaskHTML = Handlebars.templates.task(newTaskContext);
+        inputTaskLine.insertAdjacentHTML("beforebegin", newTaskHTML);
+        inputTaskLine.value = "";
+      }
+      else {
+        alert("Error storing task: " + event.target.response);
+      }
+    });
+
+    postRequest.setRequestHeader('Content-Type', 'application/json');
+    postRequest.send(requestBody);
+
   }
+});
+
+var removeButtons = document.querySelectorAll('.remove-button');
+removeButtons.forEach(function(elem) {
+  elem.addEventListener('click', function(event) {
+
+    var postRequest = new XMLHttpRequest();
+    var requestURL = '/removeTask';
+    postRequest.open('POST', requestURL);
+
+    var collection = elem.parentNode.parentNode.id;
+    if (collection == 'incomplete-tasks-list') {
+      collection = 'incomplete';
+    }
+    else if (collection == 'completed-tasks-list') {
+      collection = 'complete';
+    }
+
+    var requestBody = JSON.stringify({
+      collection: collection,
+      description: elem.parentNode.childNodes[0].textContent
+    });
+
+    postRequest.addEventListener('load', function (event) {
+      if (event.target.status === 200) {
+        elem.parentNode.remove();
+      }
+      else {
+        alert("Error removing task: " + event.target.response);
+      }
+    });
+
+    postRequest.setRequestHeader('Content-Type', 'application/json');
+    postRequest.send(requestBody);
+
+  });
+});
+
+var taskText = document.querySelectorAll('#incomplete-tasks-panel span');
+taskText.forEach(function(elem) {
+  elem.addEventListener('click', function(event) {
+
+    var description = elem.textContent;
+
+    var source = 'incomplete';
+    var destination = 'complete';
+
+    var postRequest = new XMLHttpRequest();
+    var requestURL = '/moveTask';
+    postRequest.open('POST', requestURL);
+
+    var requestBody = JSON.stringify({
+      source: source,
+      destination: destination,
+      description: description
+    });
+
+    postRequest.addEventListener('load', function (event) {
+      if (event.target.status === 200) {
+        elem.parentNode.remove();
+        var newTaskContext = {
+          description: description
+        }
+        var completedList = document.querySelector('#completed-tasks-list');
+        var newTaskHTML = Handlebars.templates.task(newTaskContext);
+        completedList.insertAdjacentHTML("beforeend", newTaskHTML);
+      }
+      else {
+        alert("Error moving task: " + event.target.response);
+      }
+    });
+
+    postRequest.setRequestHeader('Content-Type', 'application/json');
+    postRequest.send(requestBody);
+
+  });
 });
--- server.js
+++ server.js
@@ -1,29 +1,115 @@
+var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+var mongoURL = "mongodb://" + mongoUser + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort + "/" + mongoDBName;
+
 var app = express();
 var port = process.env.PORT || 3000;
-
-var incomplete = require("./incompleteTasks.json");
-var completed = require("./completedTasks.json");
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.json());
+
 app.use(express.static('public'));
 
-app.get('/', function (req, res, next) {
-  var context = {
-    incompleteTasks: incomplete,
-    completedTasks: completed
+app.post('/insertTask', function (req, res, next) {
+  if (req.body && req.body.description) {
+    var incompleteTasks = mongoDB.collection('incomplete');
+    incompleteTasks.insertOne(
+      { description: req.body.description },
+      function (err, result) {
+        if (err) {
+          res.status(500).send("Error adding task to DB");
+        }
+        else {
+          res.status(200).send("Success");
+        }
+      }
+    );
   }
-  res.render('accordion', context);
+});
+
+app.post('/removeTask', function (req, res, next) {
+  if (req.body && req.body.collection && req.body.description) {
+    var taskCollection = mongoDB.collection(req.body.collection);
+    taskCollection.deleteOne(
+      { description: req.body.description },
+      function (err, result) {
+        if (err) {
+          res.status(500).send("Error removing task from DB");
+        }
+        else {
+          res.status(200).send("Success");
+        }
+      }
+    );
+  }
+});
+
+app.post('/moveTask', function (req, res, next) {
+  if (req.body && req.body.source && req.body.destination && req.body.description) {
+    var sourceCollection = mongoDB.collection(req.body.source);
+    var destCollection = mongoDB.collection(req.body.destination);
+    sourceCollection.deleteOne(
+      { description: req.body.description },
+      function (err, result) {
+        if (err) {
+          res.status(500).send("Error removing task from DB");
+        }
+      }
+    );
+    destCollection.insertOne(
+      { description: req.body.description },
+      function (err, result) {
+        if (err) {
+          res.status(500).send("Error adding task to DB");
+        }
+      }
+    );
+    res.status(200).send("Success");
+  }
+});
+
+app.get('/', function (req, res, next) {
+  var incompleteTasks = mongoDB.collection('incomplete');
+  incompleteTasks.find({}).toArray(function(err, incompleteDoc) {
+    if (err) {
+      res.status(500).send("Error communicating with the DB");
+    }
+    var completedTasks = mongoDB.collection('complete');
+    completedTasks.find({}).toArray(function(err, completeDoc) {
+      if (err) {
+        res.status(500).send("Error communicating with the DB");
+      }
+      res.status(200).render('accordion', {
+        incompleteTasks: incompleteDoc,
+        completedTasks: completeDoc
+      });
+    });
+  });
 });
 
 app.get('*', function (req, res, next) {
   res.status(404).render('404');
 });
 
-app.listen(port, function () {
-  console.log("== Server is listening on port", port);
+MongoClient.connect(mongoURL, function(err, client) {
+  if (err) {
+    throw err;
+  }
+  mongoDB = client.db(mongoDBName);
+  app.listen(port, function () {
+    console.log("== Server listening on port", port);
+  });
 });
